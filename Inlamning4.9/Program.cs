47fd7e98f10cd63b18abf3668a967add088cbf0b
Airblow <143172177+Airblow@users.noreply.github.com>
2023-10-29 20:25:55 +0100

    Add project files.

---
+++ Inlamning4.9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace inlamning4._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur bred vill du att triangeln ska vara? Svara med en valfri siffra.");
+            int triangleWidth = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= triangleWidth; i++)
+            {
+                for (int x = 1; x <= i; x++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
